4e5bbf97e7c6a9ca1ef00c1f45950c1354d1caeb
Miguel Marquez <marquez53095@outlook.com>
2020-05-15 16:22:23 -0700

    Revision 14, created a new file called First Table View, replace it for POITableViewCell

---
+++ GrailDiary/GrailDiary/FirstTableView.swift
@@ -0,0 +1,24 @@
+//
+//  FirstTableView.swift
+//  GrailDiary
+//
+//  Created by Miguel Marquez on 5/15/20.
+//  Copyright © 2020 Miguel Marquez. All rights reserved.
+//
+
+import UIKit
+
+class FirstTableView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
